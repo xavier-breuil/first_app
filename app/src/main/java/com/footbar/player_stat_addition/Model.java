c0fae149cbc81b19c2da5e0724a1800e44e3b7ff
Xavier Breuil <breuilxavier@gmail.com>
2016-05-03 09:30:39 +0200

    print on model

--- app/src/main/java/com/footbar/player_stat_addition/Model.java
+++ app/src/main/java/com/footbar/player_stat_addition/Model.java
@@ -19,7 +19,7 @@ public class Model implements AsyncHttpObserver {
 
     public Model(Controller controller){
         this.controller = controller;
-
+        System.out.println("model");
     }
 
     @Override
